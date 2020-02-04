6bea1da02e8e6c1afaea936981a349ddfcb182f1
Leandro <leandro.frigerio@devugees.org>
2020-02-04 14:20:51 +0100

    Connected the Counter comp with MapStateToProps

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function Counter() {
+function Counter() {
     return (
         <div>
-            COUNTER HERE
+            <h2>My Redux Counter</h2>
+            <button>+</button>
+            <button>-</button>
+            <p>Counter: </p>
+            <p>Clicks:</p>
+            <p>Status:</p>
         </div>
     )
 }
+
+const MapStateToProps = state => {
+    return {
+        count: state.count,
+        click: state.click
+    }
+}
+
+export default connect(MapStateToProps)(Counter)
