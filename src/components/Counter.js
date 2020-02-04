ec98ab1c55c1ecc8dc28f779ef3329614344b4c1
Leandro <leandro.frigerio@devugees.org>
2020-02-04 14:33:55 +0100

    Dispatching action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-function Counter() {
+function Counter(props) {
+
+    const increase = () => {
+        props.dispatch({ type: "INCREASE" })
+    }
+
+    const decrease = () => {
+        props.dispatch({ type: "DECREASE" })
+    }
+
     return (
         <div>
             <h2>My Redux Counter</h2>
-            <button>+</button>
-            <button>-</button>
-            <p>Counter: </p>
-            <p>Clicks:</p>
-            <p>Status:</p>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
+            <p>Counter: {props.count}</p>
+            <p>Clicks: {props.click}</p>
+            <p>Status: {props.status}</p>
         </div>
     )
 }
@@ -17,8 +26,10 @@ function Counter() {
 const MapStateToProps = state => {
     return {
         count: state.count,
-        click: state.click
-    }
+        click: state.click,
+        status: state.status,
+        date: state.date
+    } // ok you can now use dispatch as props.dispatch
 }
 
 export default connect(MapStateToProps)(Counter)
